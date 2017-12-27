590c5517c1cfef7d46f1a79c440a2c47f820cb07
Miguel Angel Hernandez Cruz <mhernandez@unphu.edu.do>
2017-12-27 10:29:33 -0400

    Validacion en los campor de reportes

--- public/js/reporte/factura.js
+++ public/js/reporte/factura.js
@@ -8,10 +8,12 @@ $("#BuscarFacturaForm").submit(function(e){
 	var route = "/reporte/factura/"+IdFactura+"";
 	var routeDetalle = "/reporte/detalle-factura/"+IdFactura+"";
 	var contenedorDatos = $("#contenedorDatos").empty();
-	$.get(route,function(data){
-		if(data != ""){
-			$("#ContenedorTabla").show();
-			contenedorDatos.append("<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4 invoice-col'><address><strong>"+data.Paciente+"</strong><br>Cedula: "+data.Cedula+"<br>Estado: "+data.Estado+"</address></div><div class='col-xs-4 col-sm-4 col-md-4 col-lg-4 invoice-col'> <address><strong>Edad: "+data.Edad+"</strong><br>Fecha de Nacimiento<br>"+data.FechaNacimineto+"<br></address></div><div class='col-xs-4 col-sm-4 col-md-4 col-lg-4 invoice-col'><b>Numero de Seguro:</b><br>#"+data.SeguroMedico+"<br><b>Telefono:</b> "+data.Telefono+"<br></div></div><hr> ");
+
+	if($("#IdFactura").val() != ""){
+		$.get(route,function(data){
+			if(data != ""){
+				$("#ContenedorTabla").show();
+				contenedorDatos.append("<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4 invoice-col'><address><strong>"+data.Paciente+"</strong><br>Cedula: "+data.Cedula+"<br>Estado: "+data.Estado+"</address></div><div class='col-xs-4 col-sm-4 col-md-4 col-lg-4 invoice-col'> <address><strong>Edad: "+data.Edad+"</strong><br>Fecha de Nacimiento<br>"+data.FechaNacimineto+"<br></address></div><div class='col-xs-4 col-sm-4 col-md-4 col-lg-4 invoice-col'><b>Numero de Seguro:</b><br>#"+data.SeguroMedico+"<br><b>Telefono:</b> "+data.Telefono+"<br></div></div><hr> ");
 				$("#dinero").html(data.Total);
 				$("#descuentoHtml").html(data.Descuento);
 				$("#itbis").html(data.Itbis);
@@ -21,14 +23,31 @@ $("#BuscarFacturaForm").submit(function(e){
 			}else{
 				alertify.error('No hay');
 			}
-	});
-
-	$.get(routeDetalle,function(data){
-		var ProcedimientoList = $("#ProcedimientoList").empty();
-		$(data).each(function(key,value){
-			ProcedimientoList.append('<tr class="tableid"><td>'+value.IdProcedimiento+'</td><td>'+value.Procedimiento+'</td><td>'+value.Moneda+' '+value.Costo+'</td></tr>');
 		});
-	});
+
+		$.get(routeDetalle,function(data){
+			var ProcedimientoList = $("#ProcedimientoList").empty();
+			$(data).each(function(key,value){
+				ProcedimientoList.append('<tr class="tableid"><td>'+value.IdProcedimiento+'</td><td>'+value.Procedimiento+'</td><td>'+value.Moneda+' '+value.Costo+'</td></tr>');
+			});
+		});
+
+	}else{
+		var errores = "";
+		if($("#IdFactura").val() == ""){
+      $("#DivS").addClass('has-error');
+      if(errores != ""){
+        errores += "* Ingrese N. Factura.<hr>";
+      }else{
+        errores += "* Ingrese N. Factura.<hr>";
+      }
+    }else{
+      $("#DivS").removeClass('has-error');
+    }
+		if(errores!=""){
+			alertify.error(errores);
+		}
+	}
 });
 
 $("#PdfFactura").click(function(){
--- public/js/reporte/ganancia.js
+++ public/js/reporte/ganancia.js
@@ -40,44 +40,91 @@ $("#IdSucursal").change(function(e){
        IdSucursal = $(this).val();
 });
 
-$("#AgregarCliente").click(function(e){
+$("#BuscarReporteGanancia").click(function(e){
 	e.preventDefault();
 	var desde = $("#desde").val();
 	var hasta = $("#hasta").val();
-	var IdSucursal = $('#IdSucursal').val();
-	console.log(JSON.stringify(IdSucursal));
- 	var route = "/reporte-ganancia/"+IdMoneda+"/"+IdSucursal+"/"+desde+"/"+hasta+"";
-		// location = route;
-			var TablaDatos = $("#DatosReporteGanancias").empty();
+  if($("#desde").val() != "" && $("#hasta").val() != "" && IdSucursal != "" && IdMoneda != ""){
+      var route = "/reporte-ganancia/"+IdMoneda+"/"+IdSucursal+"/"+desde+"/"+hasta+"";
+      // location = route;
+      var TablaDatos = $("#DatosReporteGanancias").empty();
       var route2 = "/reporte-ganancia-total/"+IdMoneda+"/"+IdSucursal+"/"+desde+"/"+hasta+"";
       $.get(route2, function (res) {
         var total = res.Total;
         $('#totalGananciaFactura').html(total);
       });
-			$.get(route, function(data){
-				if(data != ""){
-					$(data).each(function(key,value){
-			    	TablaDatos.append('<tr><td>'+value.Sucursal+'</td><td>'+value.Caja+'</td><td>'+value.Fecha+'</td><td>'+value.IdFactura+'</td><td>'+value.Procedimiento+'</td><td class="costo">'+value.Costo+'</td></tr>')
-			    });
-			    $('#TablaDeGanancia').DataTable();
+      $.get(route, function(data){
+        if(data != ""){
+          $(data).each(function(key,value){
+            TablaDatos.append('<tr><td>'+value.Sucursal+'</td><td>'+value.Caja+'</td><td>'+value.Fecha+'</td><td>'+value.IdFactura+'</td><td>'+value.Procedimiento+'</td><td class="costo">'+value.Costo+'</td></tr>')
+          });
+          $('#TablaDeGanancia').DataTable();
 
-					var array = [];
-				$('.costo').each(function(index,object){
-					var obj = {};
-					obj['costo'] = $($(object).children()[0]).html();
-					array.push(obj);
-				});
+          var array = [];
+          $('.costo').each(function(index,object){
+            var obj = {};
+            obj['costo'] = $($(object).children()[0]).html();
+            array.push(obj);
+          });
 
-			    $("#ContenedorTabla").show();
-				}else{
-					$("#ContenedorTabla").hide();
-					swal(
-					  'No existen facturas',
-					  'En esta fechas',
-					  ''
-					)
-				}
-			});
+          $("#ContenedorTabla").show();
+        }else{
+          $("#ContenedorTabla").hide();
+          swal(
+            'No existen facturas',
+            'En esta fechas',
+            ''
+          )
+        }
+      });
+  }else{
+    var errores = "";
+    if($("#desde").val() == ""){
+      $("#DivD").addClass('has-error');
+      if(errores != ""){
+        errores += "* Seleccione fecha desde.<hr>";
+      }else{
+        errores += "* Seleccione fecha desde.<hr>";
+      }
+    }else{
+      $("#DivD").removeClass('has-error');
+    }
+    if($("#hasta").val() == ""){
+      $("#DivH").addClass('has-error');
+      if(errores != ""){
+        errores += "* Seleccione fecha hasta.<hr>";
+      }else{
+        errores += "* Seleccione fecha hasta.<hr>";
+      }
+    }else{
+      $("#DivH").removeClass('has-error');
+    }
+    if(IdSucursal == ""){
+      $("#DivS").addClass('has-error');
+      if(errores != ""){
+        errores += "* Seleccione Sucursal.<hr>";
+      }else{
+        errores += "* Seleccione Sucursal.<hr>";
+      }
+    }else{
+      $("#DivS").removeClass('has-error');
+    }
+    if(IdMoneda == ""){
+      $("#DivM").addClass('has-error');
+      if(errores != ""){
+        errores += "* Seleccione Moneda.<hr>";
+      }else{
+        errores += "* Seleccione Moneda.<hr>";
+      }
+    }else{
+      $("#DivM").removeClass('has-error');
+    }
+
+    if(errores!=""){
+			alertify.error(errores);
+		}
+
+  }
 
 	});
 
